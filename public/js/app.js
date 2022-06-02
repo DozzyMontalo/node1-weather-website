b9e2347fca7de93edbcd6c021b2f4f27c2a97c93
DozzyMontalo <dozienice@gmail.com>
2022-06-02 02:35:26 +0100

    Remove unnecessary lines of code

--- public/js/app.js
+++ public/js/app.js
@@ -1,22 +1,8 @@
-console.log('client side javascript file is loaded')
-
-fetch('http://localhost:3000/weather?address=Abuja').then((response) => {
-    response.json().then((data) =>{
-        if(data.error){
-            console.log(data.error)
-        } else{
-            console.log(data.location)
-            console.log(data.forecast)
-        }
-    })
-}) 
-
 const weatherform = document.querySelector('form')
 const search = document.querySelector('input')
 const messageOne = document.querySelector('#message-1')
 const messageTwo = document.querySelector('#message-2')
 
-// messageOne.textContent = 'From Javascript'
 
 weatherform.addEventListener('submit', (e) => {
     e.preventDefault()
@@ -29,16 +15,12 @@ weatherform.addEventListener('submit', (e) => {
     fetch('http://localhost:3000/weather?address='+ location).then((response) => {
     response.json().then((data) =>{
         if(data.error){
-            // console.log(data.error)
             messageOne.textContent = data.error
         } else{
-            // console.log(data.location)
-            // console.log(data.forecast)
             messageOne.textContent = data.location
             messageTwo.textContent = data.forecast
         }
     })
 }) 
 
-    // console.log(location)
 })
